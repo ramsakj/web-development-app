e977705a1fd1b7d289d4d850e1ed6708901bf3c5
ramsakj <jure2310@gmail.com>
2022-06-12 11:27:41 +0200

    updating runtime

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
 
-@app.route("/about")
-def about_me():
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
+
+@app.route("/about-me")
+def about():
     return render_template("about.html")
-
-@app.route("/portfolio")
-def portfolio():
-    return render_template("portfolio.html")
 
 if __name__ == '__main__':
     app.run(use_reloader=True)
